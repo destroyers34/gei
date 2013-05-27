ac5c56cf3bbd447f4a40a5d05226d2d3517edb1c
abechard <anthony@jrcmachinery.com>
2013-05-27 16:04:50 -0400

    Ajout d'un champs priorité au projet eugenie

--- intranet/projets/admin.py
+++ intranet/projets/admin.py
@@ -6,14 +6,14 @@ class ProjetEugnieAdmin(admin.ModelAdmin):
     list_filter = ('nom','modele','date_soumission','date_debut','date_fin')
     ordering = ('-actif','-numero',)
     raw_id_fields = ('client',)
-    fields = ('numero',('nom','modele'),'serial_number',('budget_mat','budget_mo'),'client',('date_soumission','date_debut','date_fin'),('actif','en_attente'))
+    fields = ('numero',('nom','modele'),'serial_number',('budget_mat','budget_mo'),'client',('date_soumission','date_debut','date_fin'),('en_attente','actif','priority'))
 
 class ProjetTPEAdmin(admin.ModelAdmin):
     list_display = ('numero', 'nom','description','serial_number','budget_mat','budget_mo','client','date_soumission','date_debut','date_fin','actif','en_attente')
     list_filter = ('nom','date_soumission','date_debut','date_fin')
     ordering = ('-actif','-numero',)
     raw_id_fields = ('client',)
-    fields = (('numero','nom'),'description','serial_number',('budget_mat','budget_mo'),'client',('date_soumission','date_debut','date_fin'),('actif','en_attente'))
+    fields = (('numero','nom'),'description','serial_number',('budget_mat','budget_mo'),'client',('date_soumission','date_debut','date_fin'),('en_attente','actif'))
     
 admin.site.register(Projet_Eugenie,ProjetEugnieAdmin)
 admin.site.register(Projet_TPE,ProjetTPEAdmin)
