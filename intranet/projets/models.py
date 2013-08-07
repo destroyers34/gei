309a751bdf3151968421d75be6aaffe84009e98a
abechard <anthony@jrcmachinery.com>
2013-08-07 07:50:22 -0400

    Ajout champ Priorité dans le display de l'admin pour projet EuGénie

--- intranet/projets/models.py
+++ intranet/projets/models.py
@@ -3,6 +3,7 @@ from django.db.models import Sum
 from clients.models import Compagnie
 from datetime import datetime
 
+
 class Projet(models.Model):
     numero = models.CharField(max_length=30,unique=True,verbose_name=u"Numéro du projet")
     nom = models.CharField(max_length=30,verbose_name=u"Nom du projet")
@@ -12,6 +13,7 @@ class Projet(models.Model):
     date_fin = models.DateField(verbose_name=u"Date de fin", default=datetime.now())
     actif = models.BooleanField(verbose_name=u"Actif")
     en_attente = models.BooleanField(verbose_name=u"En attente")
+
     def jours_restant(self):
         if self.date_fin is not None and self.date_debut is not None:
             return (self.date_fin - datetime.now().date()).days
@@ -27,12 +29,14 @@ class Projet(models.Model):
     class Meta:
             abstract = True
 
+
 class Projet_Eugenie(Projet):
     modele = models.CharField(max_length=30,verbose_name=u"Modèle")
     serial_number = models.CharField(max_length=30,verbose_name=u"Numéro de série", blank=True, null=True)
     budget_mat = models.DecimalField(max_digits=11,decimal_places=2,verbose_name=u"Budget MAT ($)",)
     budget_mo = models.DecimalField(max_digits=11,decimal_places=2,verbose_name=u"Budget MO (H)",)
     priority = models.DecimalField(max_digits=1,decimal_places=0,verbose_name=u"Priorité",default='9')
+
     def __unicode__(self):
         return u'%s : %s %s' % (self.numero, self.nom, self.modele)
         
@@ -45,16 +49,19 @@ class Projet_Eugenie(Projet):
                 return format(projets['total']*100, '.2f')    
         else:
             return format(0, '.2f')    
+
     class Meta:
         verbose_name = u"Projet EuGénie"
         verbose_name_plural = u"Projets EuGénie"
         ordering = ['-numero']
+
 
 class Projet_TPE(Projet):
     description = models.CharField(max_length=30,verbose_name=u"Description")
     serial_number = models.CharField(max_length=30,verbose_name=u"Numéro de série", blank=True, null=True)
     budget_mat = models.DecimalField(max_digits=11,decimal_places=2,verbose_name=u"Budget MAT ($)",)
     budget_mo = models.DecimalField(max_digits=11,decimal_places=2,verbose_name=u"Budget MO (H)",)
+
     def __unicode__(self):
         return u'%s : %s - %s' % (self.numero, self.nom, self.description)
     
