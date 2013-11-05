3141cda11e46d5109510e2da3c1851aaa43d2ccd
abechard <anthony@jrcmachinery.com>
2013-11-05 17:17:29 -0500

    Intégration login dans le template principale

--- intranet/intranet/views.py
+++ intranet/intranet/views.py
@@ -1,7 +1,5 @@
-﻿from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+﻿from django.shortcuts import render
 
-@login_required
+
 def index(request):
     return render(request, 'index.html')
