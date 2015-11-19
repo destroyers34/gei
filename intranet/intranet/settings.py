d910eca5be58086ffaa6f9447ef5a5151acf0e44
destroyers34 <abechard@eugeniecanada.com>
2015-11-19 13:01:07 -0500

    changed media root path

--- intranet/intranet/settings.py
+++ intranet/intranet/settings.py
@@ -3,7 +3,7 @@
 from local_settings import *
 
 ADMINS = (
-    # ('Anthony Bechard', 'anthony@jrcmachinery.com'),
+    # ('Anthony Bechard', 'abechard@eugeniecanada.com'),
 )
 
 MANAGERS = ADMINS
@@ -33,7 +33,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = join(LOCAL_PATH, 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -63,14 +63,14 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'django.contrib.staticfiles.finders.FileSystemFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,7 +79,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-	#'middleware.LastSiteUrl',
+    # 'middleware.LastSiteUrl',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -132,7 +132,7 @@ APPEND_SLASH = True
 
 GRAPPELLI_ADMIN_TITLE = 'Groupe EuGénie Admin'
 
-#GRAPPELLI_INDEX_DASHBOARD = 'intranet.dashboard.CustomIndexDashboard'
+# GRAPPELLI_INDEX_DASHBOARD = 'intranet.dashboard.CustomIndexDashboard'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = "587"
